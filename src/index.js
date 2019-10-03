853ca9834572170967014fd74bdaa55b5551e80d
tsakunova <tv.tsakunova@gmail.com>
2019-10-03 18:52:06 +0300

    finish brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let newStr = str;
+  for(let i=0; i< str.length; i++){
+    for(let j=0;j<bracketsConfig.length; j++){
+      let bracketsStr = bracketsConfig[j][0]+bracketsConfig[j][1];
+      newStr=newStr.replace(bracketsStr,"");
+    }
+  }
+  return newStr.length===0;
 }
