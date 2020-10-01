7cb76196d8dd2aca5c8c479c9ebb9cdcada957ba
tsakunova <tv.tsakunova@gmail.com>
2020-10-01 08:18:42 +0300

    brackets done

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ module.exports = function check(str, bracketsConfig) {
   let newStr = str;
   for(let i=0; i< str.length; i++){
     for(let j=0;j<bracketsConfig.length; j++){
-      let bracketsStr = bracketsConfig[j][0]+bracketsConfig[j][1];
-      newStr=newStr.replace(bracketsStr,"");
+      let bracketsStrDel = bracketsConfig[j][0]+bracketsConfig[j][1];
+      newStr=newStr.replace(bracketsStrDel,'')
     }
   }
   return newStr.length===0;
